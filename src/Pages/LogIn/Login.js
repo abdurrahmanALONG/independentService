4fa36c7129847cd00f34d5ff34a0f6472d397657
abdurrahmanALONG <abdullahhanxin@gmail.com>
2022-04-19 18:07:10 +0800

    login and registar connection compelet

--- src/Pages/LogIn/Login.js
+++ src/Pages/LogIn/Login.js
@@ -1,28 +1,46 @@
-import React from 'react';
-import { Button, Container, Form } from 'react-bootstrap';
+import React, { useRef } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
 import './Login.css'
 
 const Login = () => {
-    return (
-        <Container className='loginform'>
-            <Form>
-                <Form.Group className="mb-3 w-100" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const navigate = useNavigate();
 
-                <Form.Group className="mb-3 w-50" controlId="formBasicPassword">
+    const handlesubmit = event => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        console.log(email, password);
+
+    }
+
+    const navigateRegister = event => {
+        navigate('/Registration');
+    }
+    return (
+        <div className='container w-50 mx-auto my-5'>
+            <h2 className='text-primary text-center mt-2'>Please Login</h2>
+            <Form onSubmit={handlesubmit}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
+                </Form.Group>
+                <Form.Group className="mb-3 " controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
                 </Form.Group>
                 <Button variant="primary " type="submit">
                     Submit
                 </Button>
             </Form>
-        </Container>
+            <p>New to AR Construction? <Link to="/Registration" className='text-danger pe-auto' onClick={navigateRegister}>Please Register</Link></p>
+        </div>
+
+
+
     );
 };
 
